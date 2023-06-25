0e3048f61882dc5aec574c7cb8b1e23d91b83ad8
imranhacker69 <137699895+imranhacker69@users.noreply.github.com>
2023-06-25 20:54:53 +0600

    Add files via upload

---
+++ requirement.sh
@@ -0,0 +1,15 @@
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet Basic Installation
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+figlet -f big Done !!! | lolcat
+echo
+echo -e "[1m Now Run bash t-ban.sh...!!!"
+echo
+echo -e "[1m[32m Developed by :[33m Sutariya Parixit (8h4i)"
---
+++ t-ban.sh
@@ -0,0 +1,31 @@
+clear
+figlet -f big Termux Banner | lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
+echo -e $lightgreen "[1m                Security Help For Ethical Hackers... "
+echo " "
+echo -e $yellow "[1m                         -Sutariya Parixit "
+echo " "
+echo " "
+echo " "
+echo -e "[1m[33m
+What is Your [31mBanner [33mName[32m :
+
+"
+read varbanner
+echo
+echo -e "[1m[33m
+What is Your Cowsay Name[32m :
+
+ "
+read varcowsay
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
